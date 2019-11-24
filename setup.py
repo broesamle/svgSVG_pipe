f56dbc7aa6ad9942b97eb1217b18329ec56d56c5
Martin Brösamle <m@martinbroesamle.de>
2019-11-24 18:31:13 +0100

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="svgpipe",
-    version="0.1.0",
+    version="0.2.0",
     author="Martin Brösamle",
     author_email="m@martinbroesamle.de",
     description="Transactions on existing SVG documents: inject/extract/modify",
